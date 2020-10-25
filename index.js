3b1b34418ffe64cfcbe2806c678bd907a4a9a738
Taylor Huffman <huffmantayler@yahoo.com>
2020-10-24 23:25:31 -0400

    created procfile for heroku

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-// Stock Market Portfolio App
+// Stock Market Portfolio App By John Elder Codemy.com
 const express = require('express');
 const app = express();
-const path = require('path');
 const exphbs  = require('express-handlebars');
+const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-//Set handlebars Middleware
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// API KEY pk_062031d20883444f9ea74e2610fe2011
+// create call_api function
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_062031d20883444f9ea74e2610fe2011', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});
+};
+
+
+// Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//Set handlebar routes
+const otherstuff = "hello there, this is other stuff!";
+
+// Set handlebar index GET route
 app.get('/', function (req, res) {
-    res.render('home', {
-      stuff: "This is stuff..."
-    });
+	call_api(function(doneAPI) {
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, "fb");
+
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+			//console.log(doneAPI);
+			//posted_stuff = req.body.stock_ticker;
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, req.body.stock_ticker);
+
 });
 
 
+
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(PORT, () => console.log('Server listening on port ' + PORT));
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
